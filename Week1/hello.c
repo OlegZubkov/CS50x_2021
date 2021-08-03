9c8a83e82e4b95305b83cad6527e529f546c6453
Oleg Zubkov <ol.zubkov@gmail.com>
2021-08-03 23:01:16 +0300

    Delete Week1 directory

--- Week1/hello.c
+++
@@ -1,9 +0,0 @@
-# include <stdio.h>
-# include <cs50.h>
-
-int main(void)
-{
-    // Aksing user's name and greeting
-    string name = get_string("What's your name?\n");
-    printf("hello, %s\n", name);
-}
